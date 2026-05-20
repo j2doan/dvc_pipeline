90bc9d08d1e9e5e137b60f8b6a488e95241b8161
j2doan <j2.doan@san.rr.com>
2026-05-20 03:03:31 -0700

    fixed transformed/ dir name & uppercase event type VIEW

--- src/transform.py
+++ src/transform.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 
-os.makedirs("data/transform", exist_ok=True)
+os.makedirs("data/transformed", exist_ok=True)
 
 df = pd.read_csv("data/clean/events.csv")
 
 df["date"] = pd.to_datetime(df["timestamp"]).dt.date.astype(str)
 
-df.to_csv("data/transform/events.csv", index=False)
+df.to_csv("data/transformed/events.csv", index=False)
